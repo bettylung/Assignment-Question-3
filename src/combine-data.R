462ff86b38f7b60a1e463221f09ebf0b9c2955f3
bettylung <101471433+bettylung@users.noreply.github.com>
2022-03-15 00:38:01 +0000

    Update combine-data.R

--- src/combine-data.R
+++ src/combine-data.R
@@ -51,3 +51,8 @@ write.table(x = plot_df,
             file = "out/average-rainfall.csv",
             sep = ",",
             row.names = FALSE)
+
+sink(file = "out/package-versions.txt")
+sessionInfo()
+sink()
+
